b9ed8ed4f97bcee7136877a96e6c4b768b6aad84
Willame Oliveira <willame.oliveira@ifpi.edu.br>
2026-09-14 22:34:54 -0300

    classe conta inicial

--- intro_poo/sis_bancario/conta.py
+++ intro_poo/sis_bancario/conta.py
@@ -4,3 +4,14 @@ class Conta:
         self.numero = numero
         self.saldo = saldo
         self.cliente = cliente
+
+    def exibir_saldo(self):
+        return self.saldo
+
+    def sacar(self, valor):
+        if self.saldo > valor:
+            self.saldo -= valor
+
+    def depositar(self, valor):
+        if valor > 0:
+            self.saldo += valor
